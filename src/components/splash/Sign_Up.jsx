78785dfac638f41d100030be503f918e77c663f6
Jeffrey Ruder <ruderjt@gmail.com>
2016-05-05 10:56:50 -0700

    Initial commit

---
+++ src/components/splash/Sign_Up.jsx
@@ -0,0 +1,1 @@
+console.log('Hello sign up!');
---
+++ test/components/body/Critique_spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass,
+  Simulate
+} from 'react-addons-test-utils';
+import Critique from '../../../src/components/body/Critique';
+import {expect} from 'chai';
+
+describe('Critique', () => {
+
+  it('is a React component that has content', () => {
+    const component = renderIntoDocument(
+      <Critique />
+    );
+    const critique = scryRenderedDOMComponentsWithClass(component, "critique");
+    expect(critique[0].textContent).to.contain('Critique');
+  });
+
+});
