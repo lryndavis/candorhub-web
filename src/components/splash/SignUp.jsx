2cf2d5ec7b6f2facbd2864380bf6bc9c260e37ca
Lauryn <lryndavis@gmail.com>
2016-05-06 13:23:23 -0700

    add additional properties to SignIn form component

--- src/components/splash/SignUp.jsx
+++ src/components/splash/SignUp.jsx
@@ -2,18 +2,43 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 export default React.createClass({
+  getInitialState: function() {
+    return {value: ''};
+  },
+
+  handleChange() {
+    this.setState({value: event.target.value});
+  },
 
   render: function() {
-    return <div className="sign-up">
+    return <div className="sign-up-form">
       <p>This may or may not be a form</p>
       <label>Username:</label>
-      <input type="text" />
+      <input type="text"
+        name="username"
+        placeholder="Username"
+        value={this.state.value}
+        onChange={this.handleChange}
+        />
       <label>Email:</label>
-      <input type="text" />
+      <input type="text"
+        name="email"
+        placeholder="Email"
+        value={this.state.value}
+        onChange={this.handleChange}
+        />
       <label>Password:</label>
-      <input type="text" />
+      <input type="text"
+        name="password"
+        value={this.state.value}
+        onChange={this.handleChange}
+        />
       <label>Confirm Password:</label>
-      <input type="text"></input>
+      <input type="text"
+        name="confirm-password"
+        value={this.state.value}
+        onChange={this.handleChange}
+        />
       <button type="submit">Submit</button>
     </div>;
   }
