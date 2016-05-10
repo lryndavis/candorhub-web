4a19909a51e1688bec999fb817336dd251e74bf8
Lauryn <lryndavis@gmail.com>
2016-05-09 21:14:19 -0700

    add functioning material drawer side navigation

---
+++ src/components/body/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import ImageDehaze from 'material-ui/svg-icons/image/dehaze';
+
+export default class Sidebar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleToggle() { this.setState({open: !this.state.open}); }
+
+  handleClose() { this.setState({open: false}); }
+
+
+  render() {
+    return (
+      <div>
+        <ImageDehaze onClick={this.handleToggle} class="image-dehaze"/>
+        <Drawer
+          docked={false}
+          width={300}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+          >
+          <ContentClear onClick={this.handleClose}/>
+          <MenuItem a href="/">Home</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
